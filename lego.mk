26ef9f8692852cc455b5b3cd804420c372c363c5
anerikg <anerikgames@gmail.com>
2014-06-25 12:51:22 -0300

    Legoify
    Signed-off-by: anerikg <anerikgames@gmail.com>

--- lego.mk
+++ lego.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lego stuff.
+$(call inherit-product, vendor/lego/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -7,6 +7,6 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
+PRODUCT_NAME := lego_falcon
 
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
